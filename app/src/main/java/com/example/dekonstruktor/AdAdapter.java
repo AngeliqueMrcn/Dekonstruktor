3c6cedb0a7ed1e04303f2eae5c43b65df69c7ab2
Leslie <leslie.rineau@etu.imt-nord-europe>
2023-03-03 16:25:47 +0100

    Update AdAdapter.java

--- app/src/main/java/com/example/dekonstruktor/AdAdapter.java
+++ app/src/main/java/com/example/dekonstruktor/AdAdapter.java
@@ -5,6 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
 public class AdAdapter extends BaseAdapter {
@@ -20,19 +23,19 @@ public class AdAdapter extends BaseAdapter {
     @Override
     public int getCount() { return adModelArrayList.size() ; } // Return ad number
     @Override
-    public Object getItem(int i) { return adModelArrayList.get(i) ; } // Return ad number i
+    public AdModel getItem(int i) { return adModelArrayList.get(i) ; } // Return ad number i
     @Override
     public long getItemId(int i) { return i ; } // Return ad id i
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
 // Get ad number i
 
-        AdModel ad = adModelArrayList.getItem(i) ;
-        view = inflater.inflate(R.layout.votre_design_layout, null);
+        AdModel ad = getItem(i);
+        view = inflater.inflate(R.layout.item_listview_ad, null);
 // Get the image view and both text views
-        ImageView imageIV = view.findViewById("Mettre id image de tuile") ;
-        TextView titleTV =" ..." ;
-        TextView addressTV = "..." ;
+        ImageView imageIV = view.findViewById(R.id.imageView) ;
+        TextView titleTV = view.findViewById(R.id.textView2) ;
+        TextView addressTV = view.findViewById(R.id.textView) ;
         imageIV.setImageResource(ad.getImage());
         titleTV.setText(ad.getTitle());
         addressTV.setText(ad.getAddress());
